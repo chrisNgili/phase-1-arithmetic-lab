dc3f2d8b4fb05fd9396cedad59d1402915faf34f
chrisngili <ngilichristopher@gmail.com>
2025-03-05 14:49:33 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1*num2;
 
-// Write your code here
+let random;
+random = Math.floor(Math.random()* 10) + 1;
+
+let mod;
+let num3 = 25;
+let num4 = 7;
+mod = num3 % num4;
+
+let max;
+max = Math.max(15,1,2,3,4,5,6,20);
